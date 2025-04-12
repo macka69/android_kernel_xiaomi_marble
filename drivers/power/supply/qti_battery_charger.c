976706b19daa5fe17f74b5186a22e8af2cef4f99
Arian <arian.kulmer@web.de>
2025-04-12 13:39:06 +0200

    power: supply: qti_battery_charger: Add support for blank status and cctog xiaomi properties
    From warm-u-oss and marble OS2.0.1.0.VMRMIXM
    
    Co-authored-by: Pzqqt <821026875@qq.com>
    Change-Id: Ib15d5318a5ddee7b859f2c8e0b97832f2d59299d
    Signed-off-by: Fiqri Ardyansyah <fiqri191002@gmail.com>

--- drivers/power/supply/qti_battery_charger.c
+++ drivers/power/supply/qti_battery_charger.c
@@ -2534,6 +2534,10 @@ static void screen_state_for_charge_callback(enum panel_event_notifier_tag notif
 			default:
 				return;
 		}
+		if (write_property_id(bcdev, &bcdev->psy_list[PSY_TYPE_XM],
+				      XM_PROP_BLANK_STATUS,
+				      blank_state) < 0)
+			pr_err("%s blank state notify fail\n", __func__);
 		printk(KERN_ERR "%s, primary blank_state = %d\n", __func__, blank_state);
 		if (!bcdev->support_soc_update || bcdev->support_screen_update)
 			schedule_work(&bcdev->notify_blankstate_work);
